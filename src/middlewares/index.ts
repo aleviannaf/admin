fc1e5476771af9d5dd8c7f0348e662c25381bffb
aleviannaf <alexandrevianna511@gmail.com>
2023-08-26 17:27:45 -0400

    feat (middleware): index centraliza import of the middlewares

---
+++ src/middlewares/index.ts
@@ -0,0 +1,12 @@
+import handleErrors from "./handleErrors.middleeware"
+import ensureBody from "./ensureBody.middleware"
+import verifyEmailExist from "./verifyEmailExist.middleware"
+
+
+export default
+{
+    handleErrors,
+    ensureBody,
+    verifyEmailExist
+
+}
