b00a08e6a04f6b51364006c0fd49a788dfb84785
aleviannaf <alexandrevianna511@gmail.com>
2023-08-30 14:44:48 -0400

    feat (schemas): new schemas to check and to types of the pagination from users read

--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -18,8 +18,20 @@ const userWithoutPassword = userSchema.omit({
     password: true
 })
 
+const userReadSchema = userWithoutPassword.array()
+
+const userPaginationSchema = z.object({
+    prevPage: z.string().nullable(),
+    nextPage: z.string().nullable(),
+    currentPage: z.number().positive(),
+    totalItens: z.number().positive(),
+    data: userReadSchema
+})
+
 export {
     userSchema,
     userCreateSchema,
-    userWithoutPassword
+    userWithoutPassword,
+    userReadSchema,
+    userPaginationSchema
 }
