391f1d267da7602d0e345fbe5e5b2b5168ca23f6
aleviannaf <alexandrevianna511@gmail.com>
2023-08-29 20:47:28 -0400

    feat (middleware): insert new middleware in list

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,12 +1,15 @@
 import handleErrors from "./handleErrors.middleeware"
 import ensureBody from "./ensureBody.middleware"
 import verifyEmailExist from "./verifyEmailExist.middleware"
+import ensureTokenValid from "./ensureTokenValid.middleware"
+import verifyUserPermission from "./verifyUserPermission.middleware"
 
 
 export default
 {
     handleErrors,
     ensureBody,
-    verifyEmailExist
-
+    verifyEmailExist,
+    ensureTokenValid,
+    verifyUserPermission
 }
