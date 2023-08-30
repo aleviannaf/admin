8b69ddf3860af534993d6838e2aef1edccb18cbe
aleviannaf <alexandrevianna511@gmail.com>
2023-08-29 20:59:29 -0400

    feat (interface): route of the interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,18 @@
+import {
+    User,
+    UserRequest,
+    UserResult,
+    UserReturn
+} from "./user.interfaces"
+
+import {
+    SessionRequest
+} from "./session.interfaces"
+
+export {
+    User,
+    UserRequest,
+    UserResult,
+    UserReturn,
+    SessionRequest
+}
