b17f3082c33cec82b19eb1a35aa9982ee620396d
aleviannaf <alexandrevianna511@gmail.com>
2023-09-02 22:22:44 -0400

    update (middleware): add check if user is active

--- src/middlewares/ensureTokenValid.middleware.ts
+++ src/middlewares/ensureTokenValid.middleware.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from "express"
 import AppError from "../AppError"
 import { verify } from "jsonwebtoken"
+import { QueryConfig } from "pg"
+import { UserResult } from "../interfaces"
+import client from "../database/config"
 
-const ensureTokenValid = (
+const ensureTokenValid = async (
     request: Request,
     response: Response,
     next: NextFunction
-): Response | void => {
+): Promise<Response | void> => {
     let authorization: string | undefined = request.headers.authorization
 
     if (!authorization) {
@@ -20,6 +23,17 @@ const ensureTokenValid = (
 
         response.locals = { ...response.locals, decoded}
     })
+
+    const query: QueryConfig = {
+        text: `SELECT active FROM users WHERE id = $1;`,
+        values: [response.locals.decoded.sub]
+    }
+
+    const queryResult: UserResult = await client.query(query)
+
+    if(!queryResult.rows[0].active){
+        throw new AppError("User not exist", 400)
+    }
 
     return next()
 }
